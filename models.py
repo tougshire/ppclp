219474a602d3ab3cc916686a042d28c4ce14685f
tougshire <tougshire@tougshire.com>
2022-07-27 07:14:09 -0400

    Remove parent and add rank

--- models.py
+++ models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 class Element(models.Model):
-    title = models.CharField(
-            "element",
-            max_length=60,
-            help_text='The title of this element',
-            blank=True,
-        )
+    body = models.TextField(
+        "body",
+        blank=True,
+        help_text="The body (using Markdown) of the element"
+    )
+    is_heading = models.BooleanField(
+        'is heading',
+        default=0,
+        help_text = 'If this is a heading or a paragraph'
+    )
+    rank = models.IntegerField(
+        'rank',
+        help_text = 'The order on the list compared to siblings'
+    )
+    indent = models.IntegerField(
+        'indent',
+        default=0,
+        help_text = 'The indent level of this item'
+    )

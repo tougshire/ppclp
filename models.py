49f9bd13fdb05ef4a8c259b3c5669cf8af6402e8
tougshire <tougshire@tougshire.com>
2022-07-28 06:11:43 -0400

    change indent to level

--- models.py
+++ models.py
@@ -13,24 +13,55 @@ class Element(models.Model):
     )
     is_heading = models.BooleanField(
         'is heading',
-        default=0,
+        default=False,
         help_text = 'If this is a heading or a paragraph'
-    )
-    rank = models.IntegerField(
-        'rank',
-        help_text = 'The order on the list compared to siblings'
-    )
-    indent = models.IntegerField(
-        'indent',
-        default=0,
-        help_text = 'The indent level of this item'
     )
 
     def __str__(self):
         full_name = '%s: %s' % (self.name, self.body)
-        if full_name > 50:
+        if len(full_name) > 50:
             return full_name[:45] + ' ...'
         else:
             return full_name
 
-        
+class Page(models.Model):
+    name = models.CharField(
+        "name",
+        max_length=50,
+        help_text="The name of the page"
+    )
+    slug = models.CharField(
+        "slug",
+        max_length=50,
+        help_text="The URL slug"
+    )
+
+    def __str__(self):
+        return self.name
+
+class ElementPlacement(models.Model):
+    page = models.ForeignKey(
+        Page,
+        on_delete=models.CASCADE
+    )
+    element = models.ForeignKey(
+        Element,
+        on_delete=models.CASCADE
+    )
+    rank = models.IntegerField(
+        'rank',
+        help_text = 'The order on the page'
+    )
+    level = models.IntegerField(
+        'level',
+        default=0,
+        help_text = 'The heading level of this placement.  Applies only to headings, not paragraphs'
+    )
+
+    def __str__(self):
+        pg = self.page if hasattr(self, 'page') else "Undefined"
+        el = self.element if hasattr(self, 'element') else "Undefined"
+        return '%s on page %s' % (el, pg)
+
+    class Meta:
+        ordering = ['rank']

d7ca0d4e9bf4b20e889a902964b9eac03256900c
tougshire <tougshire@tougshire.com>
2022-07-30 17:11:12 -0400

    add code and css for headings inside content

--- views.py
+++ views.py
@@ -13,24 +13,31 @@ class PageDetail(DetailView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         context_data['elements'] = context_data['page'].element_set.all()
-        for el in context_data['elements']:
-            lv = str(el.level)
-            el.title = '<h' + lv + '>' + el.title + '</h' + lv + '>'
-            el.content = '<div class="content-block">' + markdown.markdown(el.content) + '</div>'
+        for element in context_data['elements']:
+            element_level_str = str(element.level)
+            element.title = '<h' + element_level_str + '>' + element.title + '</h' + element_level_str + '>'
+            element.content = '<div class="content-block">' + markdown.markdown(element.content) + '</div>'
 
             #Push down heading levels within the content so a heading within content is lower than the heading
             #of the element.  So if the elemnent is level 2, then title is enclosed in h2 tags, and the following code
             #converts h1 tags in the content to h3
             for i in range(6,0,-1):
                 for header_tag in ['<h', '</h']:
-                    header_level = i + el.level
+
+                    header_level = i + element.level
+                    header_level_str = str(header_level)
+                    i_str = str(i)
+
                     if header_level > 6:
                         if header_tag == '<h':
-                            el.content = el.content.replace(header_tag + str(i) + '>', '<h6 class="h' + str(header_level) + '">')
+                            element.content = element.content.replace(header_tag + i_str + '>', '</div><h6 class="h' + header_level_str + '">')
                         else:
-                            el.content = el.content.replace(header_tag + str(i) + '>', '</h6>')  
+                            element.content = element.content.replace(header_tag + i_str + '>', '</h6><div class="content-block">')  
                     else:
-                        el.content = el.content.replace(header_tag + str(i) + '>', '</div>' + header_tag + str(header_level) + '><div class="content-block">')
+                        if header_tag == '<h':
+                            element.content = element.content.replace(header_tag + i_str + '>', '</div>' + header_tag + header_level_str + '>')
+                        else:
+                            element.content = element.content.replace(header_tag + i_str + '>', header_tag + header_level_str + '><div class="content-block">')
 
         return context_data
         

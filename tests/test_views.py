6d84fce413af8ad6f02531ff747132d388a9ee60
tougshire <tougshire@tougshire.com>
2022-07-30 07:27:09 -0400

    create test for markdown in detail_view

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,7 +9,7 @@ from django.contrib import admin
 class TestContentConversion(TestCase):
     def setUp(self):
         self.page_one = Page.objects.create(name='First Page')
-        self.element_one =Element.objects.create(page=self.page_one, title="Element One", level=2, content="# this should h3 \n\n plain ***em***")
+        self.element_one =Element.objects.create(page=self.page_one, title="Element One", level=2, content="# this should be h3 \n\n plain ***em***")
         self.element_two =Element.objects.create(page=self.page_one, title="Element Two", level=3, content="# this should be h4 \n\n plain ***em***")
         self.element_three =Element.objects.create(page=self.page_one, title="Element Three", level=2, content="## this should be h4 \n\n plain ***em***")
         self.element_four =Element.objects.create(page=self.page_one, title="Element Four", level=4, content="## this should be h6 \n\n plain ***em***")
@@ -19,6 +19,17 @@ class TestContentConversion(TestCase):
 
     def test_check_markdown(self):
         view = PageDetail(object=self.page_one)
-        print('tp227tl12', view.get_context_data()['elements'])
-        print('tp227u701', view.get_context_data()['elements'].get(title="Element One").content)        
-        print('tp227u702', view.get_context_data()['elements'].get(title="Element Two").content)        
+        elements = view.get_context_data()['elements']
+        test_string='<h3>this should be h3</h3>'
+        self.assertEqual(elements[0].content[:len(test_string)],test_string)
+        test_string='<h4>this should be h4</h4>'
+        self.assertEqual(elements[1].content[:len(test_string)],test_string)
+        test_string='<h4>this should be h4</h4>'
+        self.assertEqual(elements[2].content[:len(test_string)],test_string)
+        test_string='<h6>this should be h6</h6>'
+        self.assertEqual(elements[3].content[:len(test_string)],test_string)
+        test_string='<h6 class="h7">this should be h6 class h7</h6>'
+        self.assertEqual(elements[4].content[:len(test_string)],test_string)
+        test_string='<h6 class="h7">this should be h6 class h7</h6>'
+        self.assertEqual(elements[5].content[:len(test_string)],test_string)
+
